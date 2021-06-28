a74eb04e426f273531bbe44c0cec70a8b10e4e73
Manav Patel <84576118+manaviyu@users.noreply.github.com>
2021-06-28 10:37:51 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>ReactJS and Javascript Bootcamp</h1>
+      <p>venue : @shapeai youtube channel.</p>
+    </div>
+  );
+}
+
+export default Info;
